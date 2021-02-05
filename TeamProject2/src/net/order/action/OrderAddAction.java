3c64290856135ee432ea78d6f5971e4e4b4afb54
jiwon_lee <wldnjs5842@naver.com>
2021-02-05 12:22:14 +0900

    18. OrderAddAction 넘길 파라미터값 추가. jsp 파일 이동.

--- TeamProject2/src/net/order/action/OrderAddAction.java
+++ TeamProject2/src/net/order/action/OrderAddAction.java
@@ -35,13 +35,22 @@ public class OrderAddAction implements Action {
 		// OrderBean 객체 생성 -> 정보 저장
 		OrderBean ob =  new OrderBean();
 		ob.setId(id);
-		ob.setB_name(request.getParameter("b_name")); //b_name이 없음. b_num을 넘기는것인지??
+		ob.setB_num(Integer.parseInt(request.getParameter("b_num")));
+		ob.setO_name(request.getParameter("o_name"));
+		ob.setO_phone(request.getParameter("o_phone"));
+		ob.setO_email(request.getParameter("o_email"));
+		ob.setP_num(Integer.parseInt(request.getParameter("p_num")));
+		ob.setB_count(Integer.parseInt(request.getParameter("b_count")));
+		ob.setPoint(Integer.parseInt(request.getParameter("point")));
+		ob.setD_cost(Integer.parseInt(request.getParameter("d_cost")));
+		ob.setD_result(Integer.parseInt(request.getParameter("d_result")));
 		ob.setD_name(request.getParameter("d_name"));
 		ob.setD_phone(request.getParameter("d_phone"));
+		ob.setD_postcode(Integer.parseInt(request.getParameter("postcode")));
 		ob.setD_address(request.getParameter("d_address"));
 		ob.setD_address2(request.getParameter("d_address2"));
 		ob.setD_message(request.getParameter("d_message"));
-
+		ob.setPayment(Integer.parseInt(request.getParameter("payment")));
 		
 		CartDAO cdao = new CartDAO();
 		Vector data = cdao.getCartList(id);
@@ -64,11 +73,11 @@ public class OrderAddAction implements Action {
 		System.out.println("M : 구매확정 내역 전송 완료");
 		
 		//상품정보 수정 - 구매 수량만큼 구매갯수 차감
-		ProductDAO pdao = new ProductDAO(); 
-		pdao.updateAmount(cartList);
+		ProductDAO pdao = new ProductDAO(); //해당하는 ProductDAO에 아래 updateAmount 넘기기
+		pdao.updateAmount(cartList); //product테이블의 p_count(재고) - p_order의 b_count(실제 구매 수량) 하는 형식으로 제품 재고 update할 것
 		
 		//장바구니 비우기(삭제)
-		cdao.deleteCart(id);
+		cdao.deleteCart(id); //b_num값 넘기는지?
 		
 		//페이지이동
 		forward.setPath("./OrderList.or");
