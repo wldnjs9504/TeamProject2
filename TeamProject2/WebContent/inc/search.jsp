4d0962ec4a20cf626ed587f193161296428373f7
jiwon_lee <wldnjs5842@naver.com>
2021-02-10 12:35:31 +0900

    32.로그인 후 보고있던 페이지로 돌아가도록 설정. (새로고침1회 필요)

--- TeamProject2/WebContent/inc/search.jsp
+++ TeamProject2/WebContent/inc/search.jsp
@@ -15,14 +15,6 @@
 <div class="advanced-search">
 	<form action="./ProductList.p?" method="get" name="searF"> 
 		<input type="hidden" name="category" value="0">
-		<!-- 
-		<select class="category-btn" name="sorting">
-			<option value="num_desc" selected="selected">최근등록순</option>
-			<option value="star_avg">별점순</option>
-			<option value="price_high">가격높은순</option>
-			<option value="price_low">가격낮은순</option>
-			<option value="readcount">조회순</option>
-		</select>  -->
 		<div class="input-group">
 		
 		<%
@@ -34,7 +26,6 @@
 		<%
 		}
 		%>
-			<!-- 수정 :  검색 아이콘이 너무 앞에 있음-->
 			<button type="submit"><i class="ti-search"></i></button>
 		</div>
 	</form>
--- TeamProject2/src/net/member/action/MemberLoginAction.java
+++ TeamProject2/src/net/member/action/MemberLoginAction.java
@@ -68,12 +68,23 @@ public class MemberLoginAction implements Action {
 	    
 	    // 메인 페이지로 이동
 	    // 페이지 이동 - ActionForward 객체 
-	    ActionForward forward = new ActionForward();
-	    forward.setPath("./Main.me");
-	    forward.setRedirect(true);
+//	    ActionForward forward = new ActionForward();
+//	    forward.setPath("./Main.me");
+//	    forward.setRedirect(true);
 
+    	out.println("<script>");
+    	out.println("alert('로그인 성공');");
+    	out.println(" history.go(-2); ");
+//    	out.println(" location.href=document.referrer; ");
+    	out.println("location.reload();");
+
+    	out.println("</script>");
+    	
+    	out.close();
+    		
+    	return null; // 메서드 종료 -> 컨트롤러 이동X
+	    
 	    // 이동정보를 리턴(컨트롤러에서 이동)
-		return forward;
 	}
 
 }
